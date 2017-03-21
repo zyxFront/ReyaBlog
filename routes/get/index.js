4fd4de5cf0779068ae1f8e5397025eeb3692e063
zyxFront <1293485130@qq.com>
2017-03-21 20:44:41 +0800

    connect sql

--- routes/get/index.js
+++ routes/get/index.js
@@ -2,25 +2,18 @@
 
 let handlebars 	= require('handlebars');
 let config 		= require('../../config');
+let mongoose 	= require("mongoose");
+let Article 	= require("../../sql/models/Article");
+
+mongoose.connect("mongodb://127.0.0.1:27017/ReyaBlog");
 
 module.exports = (req,res)	=> {
-	console.log('start');
-	var articles = [{
-		title:'【声音江】解花语',
-		imgUrl:'/static/images/flower.jpg',
-		summary:'人们总是这样，喜新厌旧，疯狂地喜欢着新鲜的东西，比如芊芊的舞。因为爱而自我怀疑的人，你永远不知道你有多美。',
-		url:'/id'
-	},{
-		title:'【声音江】解花语',
-		imgUrl:'/static/images/flower.jpg',
-		summary:'人们总是这样，喜新厌旧，疯狂地喜欢着新鲜的东西，比如芊芊的舞。因为爱而自我怀疑的人，你永远不知道你有多美。',
-		url:'/id'
-	},{
-		title:'【声音江】解花语',
-		imgUrl:'/static/images/flower.jpg',
-		summary:'人们总是这样，喜新厌旧，疯狂地喜欢着新鲜的东西，比如芊芊的舞。因为爱而自我怀疑的人，你永远不知道你有多美。',
-		url:'/id'
-	}];
-		
-	res.send(config.html.index({articles:articles}));
+	Article.find({},function(err,articles){
+		if(err){
+			console.log(err);
+			res.send('Error:',err);
+		}else{
+			res.send(config.html.index({articles:articles}));
+		}
+	});
 }
